ac46c3057955c1599091588d256462d37f69a807
Verox001 <altergeist987@gmail.com>
2021-02-05 16:47:13 +0100

    Update RoundListener.java

--- src/main/java/de/cimeyclust/listener/RoundListener.java
+++ src/main/java/de/cimeyclust/listener/RoundListener.java
@@ -1,6 +1,11 @@
 package de.cimeyclust.listener;
 
+import cn.nukkit.Player;
+import cn.nukkit.block.Block;
+import cn.nukkit.blockentity.BlockEntitySign;
+import cn.nukkit.event.EventHandler;
 import cn.nukkit.event.Listener;
+import cn.nukkit.event.player.PlayerInteractEvent;
 import de.cimeyclust.BedWars;
 
 public class RoundListener implements Listener
@@ -11,5 +16,26 @@ public class RoundListener implements Listener
         this.plugin = plugin;
     }
 
-
+    @EventHandler
+    public void onPlayerJoinRoundEvent(PlayerInteractEvent event)
+    {
+        Block block = event.getBlock();
+        Player player = event.getPlayer();
+        if(block.getLevel().getBlockEntity(block.getLocation()) instanceof BlockEntitySign) {
+            BlockEntitySign blockEntitySign = (BlockEntitySign) block.getLevel().getBlockEntity(block.getLocation());
+            String roundName = null;
+            for(String name : this.plugin.getBedWarsAPI().getNames())
+            {
+                if(this.plugin.getBedWarsAPI().getJoinSign(name).getLocation().equals(blockEntitySign) && blockEntitySign.getText()[0].contains("BedWars"))
+                {
+                    break;
+                }
+                roundName = name;
+            }
+            if(roundName != null && this.plugin.getRoundManager().checkIfRoundExists(roundName))
+            {
+                player.sendMessage(roundName);
+            }
+        }
+    }
 }
